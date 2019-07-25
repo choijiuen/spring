ad727d224dba9c786b7bfbe30954702713f692a2
choijiuen <choijiuen@gmail.com>
2019-07-25 16:14:46 +0900

    7월 25일 목요일 4시14분

---
+++ sts02/src/main/java/com/bit/day02/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.bit.day02.controller;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+//	Logger log= Logger.getLogger(IndexController.class);
+//	Logger log= Logger.getLogger("com.bit.day02.controller.IndexController.class");
+	Logger log= Logger.getLogger(this.getClass());
+	
+	public IndexController() {
+		log.debug("index Create...");
+	}
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		log.debug("index...");
+		ModelAndView mav =new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+
+}
